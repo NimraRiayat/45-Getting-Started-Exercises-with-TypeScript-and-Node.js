eacdd2c736f8ddfe62436003532bfe76db063bd2
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:50:39 +0500

    task 21

---
+++ task21.js
@@ -0,0 +1,6 @@
+"use strict";
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects
+containing these items.*/
+let arr = { name: "Ali", age: "9" };
+console.log(arr.name);
+console.log(arr.age);
---
+++ task21.ts
@@ -0,0 +1,6 @@
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects 
+containing these items.*/
+
+let arr={name:"Ali", age:"9"}
+console.log(arr.name);
+console.log(arr.age);
