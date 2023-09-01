dc66dcdfa5b6e5c79031a6b7a81a986e3d1eec70
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:45:32 +0500

    task 10

---
+++ task10.js
@@ -0,0 +1,9 @@
+"use strict";
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point, just
+ add your name and the current date at the top of each program file. Then write one sentence describing
+  what the program does.*/
+//my name is xyz , 8/10/2023
+var famous_person = "Imran Khan";
+var message = " \"Leader leads people.Leader does not follow public opinion.\" ";
+console.log(famous_person + " once said \," + message); //this program prints the message of Imran Khan.
