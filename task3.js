caf0d12e01c30a865a26e585c154e255f1ee6fbc
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:42:26 +0500

    task 3

---
+++ task3.js
@@ -0,0 +1,7 @@
+"use strict";
+/*Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase,
+ and titlecase*/
+let name1 = "alBErt";
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
