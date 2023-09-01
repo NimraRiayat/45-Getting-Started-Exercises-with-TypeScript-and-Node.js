6e0c139a0cec3ebc0a943d52eb083b1e40af4a5f
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:51:07 +0500

    task 22

---
+++ task22.js
@@ -0,0 +1,9 @@
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make
+ one happen. Change an index in one of your programs to produce an index error. Make sure you correct the
+ error before closing the program.*/
+//index error intentionally
+var mountain = ["k2", "everest", "karakorum"];
+console.log(mountain[3]); // undefined
+var mountain_crr = ["k2", "everest", "karakorum"];
+console.log(mountain[2]); // karakorum
+// error corrected
---
+++ task22.ts
@@ -0,0 +1,13 @@
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make
+ one happen. Change an index in one of your programs to produce an index error. Make sure you correct the 
+ error before closing the program.*/
+
+ //index error intentionally
+let mountain=["k2","everest","karakorum"]; 
+console.log(mountain[3]);      // undefined
+
+
+let mountain_crr=["k2","everest","karakorum"];
+console.log(mountain[2]);  // karakorum
+ // error corrected
+
