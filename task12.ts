fce9ff95bf1cad204cedbc86c02840856b60a7ab
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:46:35 +0500

    task 12

---
+++ task12.ts
@@ -0,0 +1,8 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+ print a message to them. The text of each message should be the same, but each message should be personalized
+  with the person’s name.*/
+
+let names=["mark", "John","Kim"];
+console.log(names[0] + " How you doing? ");
+console.log(names[1] + " How you doing? ");
+console.log(names[2] + " How you doing? ");
