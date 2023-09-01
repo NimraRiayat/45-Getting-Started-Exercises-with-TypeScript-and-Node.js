87d59a4ec75dcc9c4e19bf3276bba367b470d83e
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:47:29 +0500

    task 14

---
+++ task14.js
@@ -0,0 +1,9 @@
+"use strict";
+/*Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list
+ that includes at least three people you’d like to invite to dinner. Then use your list to print a message
+  to each person, inviting them to dinner*/
+let guests = ["John", "Shake", "Mike"];
+console.log(guests);
+console.log(guests[0] + "! I would like to invite you to dinner.");
+console.log(guests[1] + "! I would like to invite you to dinner.");
+console.log(guests[2] + "! I would like to invite you to dinner.");
