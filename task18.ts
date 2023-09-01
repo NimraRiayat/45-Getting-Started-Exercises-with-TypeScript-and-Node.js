2f6d2c5f8fb4751fa749c64406b31f216785c50f
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:49:19 +0500

    task 18

---
+++ task18.ts
@@ -0,0 +1,36 @@
+//Seeing the World: Think of at least five places in the world you’d like to visit.
+// Store the locations in a array. Make sure the array is not in alphabetical order.
+let place=["france","spain","uk","japan","brazil"];
+
+//• Print your array in its original order.
+console.log(place);
+
+//Print your array in alphabetical order without modifying the actual list.
+console.log([...place].sort());
+
+//• Show that your array is still in its original order by printing it.
+console.log(place);
+
+//• Print your array in reverse alphabetical order without changing the order of the original list.
+console.log([...place].reverse());
+
+//Show that your array is still in its original order by printing it again.
+console.log(place);
+
+// Reverse the order of your list. Print the array to show that its order has changed.
+console.log(place.reverse());
+
+//Reverse the order of your list again. Print the list to show it’s back to its original order.
+console.log(place.reverse());
+
+// Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log(place.sort());
+
+/*Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order 
+has changed.*/
+console.log(place.reverse());
+
+
+
+
+
