d0aad309f0c4160e42be59db8cf2f14fc6f8fd50
NimraRiayat <141768505+NimraRiayat@users.noreply.github.com>
2023-09-01 12:49:47 +0500

    task 19

---
+++ task19.ts
@@ -0,0 +1,5 @@
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message 
+indicating the number of people you are inviting to dinner.*/
+
+let guests=["John","Shake","Mike"];
+console.log(guests.length);
